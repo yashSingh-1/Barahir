543375ec32be20ca0f09baeacf6c12007719b7ed
Yash Singh <dobbyneedsclothes@gmail.com>
2024-10-26 11:52:02 +0530

    setting up homepage

---
+++ src/app/(website)/layout.tsx
@@ -0,0 +1,14 @@
+
+type Props = {
+    children: React.ReactNode
+}
+
+const layout = ({children}: Props) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default layout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs"
+import { Manrope } from "next/font/google"
+import { ThemeProvider } from "@/components/theme";
+
+const manrope = Manrope({
+  subsets: ['latin']
+})
 
 
 export const metadata: Metadata = {
   title: "Barahir",
-  description: "Communicate with your Users Directly!",
+  description: "Share AI powered videos with Users Directly!",
 };
 
 export default function RootLayout({
@@ -17,9 +23,15 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body
-        className=''
+        className={`${manrope.className} bg-[#171717] flex justify-center items-center h-screen`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
         {children}
+        </ThemeProvider>
       </body>
     </html>
     </ClerkProvider>
